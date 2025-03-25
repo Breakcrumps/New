de3b9d5752f63106828315ca5ead9153de3e5e72
Breakcrumps <sonickita7@gmail.com>
2025-03-25 18:58:43 +0300

    Firset contact on enemy force

--- ConsoleApp1/Managers/TurnManagers/PlayableTurnManager.cs
+++ ConsoleApp1/Managers/TurnManagers/PlayableTurnManager.cs
@@ -5,12 +5,54 @@ namespace Managers;
 
 public readonly struct PlayableTurnManager : ITurnManager
 {
-  public async Task ExecuteTurn(Character character, List<Character> activeCharacters)
+  public async Task ExecuteTurn(Character caller, List<Character> activeCharacters)
   {
     DebugReporter.ReportPlayerTurnStart();
     
-    Reporter.ReportActiveCharacters(character, activeCharacters);
+    Reporter.ReportActiveCharacters(caller, activeCharacters);
+    
+    Reporter.PresentTurnOptions();
 
-    await Task.Run(ReadLine);
+    string directive = await GetNextDirective();
+
+    if (directive == "Attack")
+    {
+      await Attack(caller, activeCharacters);
+    }
+  }
+  private async Task<string> GetNextDirective()
+  {
+    string? input = await Task.Run(() => ReadLine()!.Trim());
+
+    while (!Reporter.PlayerTurnOptions.Any(kvp => kvp.Value.Contains(input)))
+    {
+      Reporter.Report("Come again?");
+      input = await Task.Run(() => ReadLine()!.Trim());
+    }
+
+    string directive = Reporter.PlayerTurnOptions.FirstOrDefault(kvp => kvp.Value.Contains(input)).Key;
+
+    return directive!;
+  }
+
+  private async Task Attack(Character caller, List<Character> activeCharacters)
+  {
+    Reporter.Report("Whom to attack?");
+    Reporter.Report("\tPlease enter the enemy's number or 0 to return.");
+
+    string input = await Task.Run(() => ReadLine()!.Trim());
+    int enemyNumber;
+
+    while (
+      !int.TryParse(input, out enemyNumber)
+      || (enemyNumber - 1) >= activeCharacters.Count
+      || (enemyNumber - 1) < -1
+    )
+    {
+      Reporter.Report("Come again?");
+      input = await Task.Run(() => ReadLine()!.Trim());
+    }
+
+    caller.Attack(activeCharacters[enemyNumber - 1]);
   }
 }
--- ConsoleApp1/Static/Reporter.cs
+++ ConsoleApp1/Static/Reporter.cs
@@ -5,6 +5,13 @@ namespace Static;
 
 public static class Reporter
 {
+  public static Dictionary<string, string[]> PlayerTurnOptions => new()
+  {
+    ["Attack"] = ["Attack", "1"],
+    ["Shield"] = ["Shield", "2"],
+    ["Wait"] = ["Wait", "None", "3"]
+  };
+
   public static void ReportAttack(float damage, Character subject)
   {
     Report($"{subject.Name} was hit for {damage}HP, {subject.Health}HP left");
@@ -33,6 +40,14 @@ public static class Reporter
       Report($"{i + 1}. {activeCharacter.Name}\t{activeCharacter.Health}HP\t{activeCharacter.Team.Name}{(activeCharacter == caller ? "\t<-- [YOU]" : "")}");
     }
   }
+  public static void PresentTurnOptions()
+  {
+    Report($"What will you do?");
+    Report($"1 or Attack: Attack an enemy.");
+    Report($"2 or Shield: Shield against incoming attacks for a turn.");
+    Report($"3 or None or Wait: Do nothing for the turn.");
+  }
+
   public static void Report(string message)
   {
     WriteLine($"\n{message}");
